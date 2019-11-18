58ecfc6715af3eedf103785fbf4e679389e419b2
Ryan Lee <ryanjlee@berkeley.edu>
2019-11-18 13:16:33 -0800

    Fix indents

--- popup.js
+++ popup.js
@@ -6,20 +6,21 @@
 var bkg = chrome.extension.getBackgroundPage();
 
 document.getElementById('inp').addEventListener('keyup', (event) => {
-	var canvas = document.getElementById('canvas'),
+  var canvas = document.getElementById('canvas'),
   ctx = canvas.getContext('2d');
 	let memeText = event.target.value.split('').map(function(v) {
-  var chance = Math.round(Math.random());
-  return v = chance ? v.toUpperCase() : v.toLowerCase();
+    var chance = Math.round(Math.random());
+    return v = chance ? v.toUpperCase() : v.toLowerCase();
 	}).join('');
-	var imageObj = new Image();
-    imageObj.onload = function(){
-    	ctx.drawImage(imageObj, 0, 0);
-      ctx.font = "15pt Arial";
-      ctx.fillText(memeText, 10, 20);
-			document.getElementById('dis_img').src = canvas.toDataURL();
-			document.getElementById('dis_img').style.display = "block";
-     };
+	
+  var imageObj = new Image();
+  imageObj.onload = function(){
+    ctx.drawImage(imageObj, 0, 0);
+    ctx.font = "15pt Arial";
+    ctx.fillText(memeText, 10, 20);
+		document.getElementById('dis_img').src = canvas.toDataURL();
+		document.getElementById('dis_img').style.display = "block";
+  };
   imageObj.src = "./meme.jpg"; 
 });
 
