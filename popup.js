88c2517b9ab4f42a24e45531f478f1c5b22c398a
Ryan Lee <ryanjlee@berkeley.edu>
2019-11-19 18:55:16 -0800

    Fix tabs

--- popup.js
+++ popup.js
@@ -10,52 +10,52 @@ var fontSize = "15px"
 
 // On keyup, add the text to the meme pic
 document.getElementById('input_text').addEventListener('keyup', (event) => {
-	let memeText = convertToMemeText(event.target.value) 
+  let memeText = convertToMemeText(event.target.value) 
   var imageObj = new Image();
   imageObj.onload = function(){
     ctx.drawImage(imageObj, 0, 0);
     ctx.font = "15pt Arial";
-		var lines = fragmentText(memeText, width - parseInt(fontSize));
-		lines.forEach(function(line, i) {
+    var lines = fragmentText(memeText, width - parseInt(fontSize));
+    lines.forEach(function(line, i) {
         ctx.fillText(line, parseInt(fontSize)/2, (i + 1) * parseInt(fontSize,0) + 1);
     });
-		ctx.restore();
-		document.getElementById('dis_img').src = canvas.toDataURL();
-		document.getElementById('dis_img').style.display = "block";
-		document.getElementById('help_text').style.display = "block";
+    ctx.restore();
+    document.getElementById('dis_img').src = canvas.toDataURL();
+    document.getElementById('dis_img').style.display = "block";
+    document.getElementById('help_text').style.display = "block";
   };
   imageObj.src = "./meme.jpg"; 
 });
 
 // Helper functions
 function fragmentText(text, maxWidth) {
-	var words = text.split(' '),
-  	lines = [],
-   	line = "";
- 	if (ctx.measureText(text).width < maxWidth) {
-  	return [text];
+  var words = text.split(' '),
+    lines = [],
+    line = "";
+  if (ctx.measureText(text).width < maxWidth) {
+    return [text];
   }
- 	while (words.length > 0) {
-  	while (ctx.measureText(words[0]).width >= maxWidth) {
-    	var tmp = words[0];
-			words[0] = tmp.slice(0, -1);
-			if (words.length > 1) {
-				words[1] = tmp.slice(-1) + words[1];
-			} else {
-				words.push(tmp.slice(-1));
-			}
-		}
-		if (ctx.measureText(line + words[0]).width < maxWidth) {
-			line += words.shift() + " ";
-		} else {
-			lines.push(line);
-			line = "";
-		}
-		if (words.length === 0) {
-			lines.push(line);
-		}
-	}
-	return lines;
+  while (words.length > 0) {
+    while (ctx.measureText(words[0]).width >= maxWidth) {
+      var tmp = words[0];
+      words[0] = tmp.slice(0, -1);
+      if (words.length > 1) {
+        words[1] = tmp.slice(-1) + words[1];
+      } else {
+        words.push(tmp.slice(-1));
+      }
+    }
+    if (ctx.measureText(line + words[0]).width < maxWidth) {
+      line += words.shift() + " ";
+    } else {
+      lines.push(line);
+      line = "";
+    }
+    if (words.length === 0) {
+      lines.push(line);
+    }
+  }
+  return lines;
 }
 
 function convertToMemeText(text) {
